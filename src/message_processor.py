fc694bd82bebbb6f330f234db9e609ddcfa18854
Anakai Richards <anakai.richards@gmail.com>
2025-03-16 06:43:07 -0500

    refactor: try to run thread as daemon

--- src/message_processor.py
+++ src/message_processor.py
@@ -1,3 +1,14 @@
+import os
+from threading import Thread
+from config import app
 from event.config import listen_for_messages
 
-listen_for_messages()
+thread = Thread(target=listen_for_messages)
+
+thread.daemon = True
+thread.start()
+
+if(__name__ == "__main__"):
+    app.run(debug= os.environ.get("IS_DEBUG") == "True", 
+        host= os.environ.get('HOST'),
+        port=os.environ.get('PORT'))
