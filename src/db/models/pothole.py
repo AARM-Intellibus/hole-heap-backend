112d9213ea7ca0905d039160aec0256cae245f9e
Anakai Richards <anakai.richards@gmail.com>
2025-03-16 13:00:15 -0500

    refactor: updates

--- src/db/models/pothole.py
+++ src/db/models/pothole.py
@@ -7,12 +7,11 @@ from sqlalchemy.ext.hybrid import hybrid_property
 class Pothole(db.Model):
     __tablename__ = 'pothole'
     id = db.Column(db.Uuid, primary_key=True, default=uuid4)
-    created_by_uid = db.Column(db.String(50),nullable=False)
+    user_id = db.Column(db.String(50),nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     danger_level = db.Column(db.String(10), nullable=False)
-    street_name = db.Column(db.String(100), nullable=True)
     image = db.Column(db.String, nullable=False)
 
     @hybrid_property
