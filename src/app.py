1af89db2604672128d2295fa271036b9f219d145
Anakai Richards <anakai.richards@gmail.com>
2025-03-15 19:45:51 -0500

    refactor: add routes

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+jwt = JWTManager(app)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
